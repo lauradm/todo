80612d204314e4716dc8a672c815d3f03d3a2fc0
albertolamana <albertolamana@gmail.com>
2015-04-20 17:08:27 +0200

    add ajax nueva tarea

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -32,6 +32,7 @@ class TasksController < ApplicationController
       if @task.save
         format.html { redirect_to @task, notice: 'Task was successfully created.' }
         format.json { render :show, status: :created, location: @task }
+        format.js { render :create }
       else
         format.html { render :new }
         format.json { render json: @task.errors, status: :unprocessable_entity }
